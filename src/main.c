7bfbfc5721f6ec05e92af10b42a8b305b93c3992
tuildes <frehsegustavo@gmail.com>
2024-10-31 16:52:20 -0300

    feat: codec de musica e sons

--- src/main.c
+++ src/main.c
@@ -2,52 +2,51 @@
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
 
 #include "auxiliar.h"
-
-#define X_SCREEN 960 // 16 * 60
-#define Y_SCREEN 540 // 9 * 60
+#include "player.h"
 
 #define KEY_SEEN     1
 #define KEY_RELEASED 2
 
 int main(void) {   
 
-    init_test(al_init(), "Allegro"); // Inicializacao do allegro
-    init_test(al_install_keyboard(), "teclado");
-    init_test(al_init_font_addon(), "addon de font");
-    init_test(al_init_ttf_addon(), "addon de ttf");
-    init_test(al_init_image_addon(), "addon de imagens");
+    init_all();
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 30.0); // Tempo de atualizacao - FPS (Frames por segundo)
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue(); 
-    ALLEGRO_FONT* font = al_load_ttf_font("assets/font/25_Sydnie.ttf", 20, ALLEGRO_ALIGN_LEFT); //NOW WE CAN CHOOSE SIZE
+    ALLEGRO_FONT* font = al_load_ttf_font("assets/font/25_Sydnie.ttf", 15, ALLEGRO_ALIGN_LEFT); //NOW WE CAN CHOOSE SIZE
     ALLEGRO_DISPLAY* disp = al_create_display(X_SCREEN, Y_SCREEN); // Tamanho da janelas
     
     // Inicializar sprites principais
-    ALLEGRO_BITMAP* player = init_bitmap("assets/sprite/player_idle.png");
+    Player player = create_player(50.0, 50.0);
+    ALLEGRO_AUDIO_STREAM* music = al_load_audio_stream("assets/sound/0_music.opus", 2, 2048);
+    init_test(music, "musica de fundo");
+    al_set_audio_stream_playmode(music, ALLEGRO_PLAYMODE_LOOP);
+    al_set_audio_stream_gain(music, ((float) 0.01)); // Volume da musica
+    al_attach_audio_stream_to_mixer(music, al_get_default_mixer());
+
+    // Setar o nome e icone da janela
     ALLEGRO_BITMAP* icon = init_bitmap("assets/favicon.png");
+    init_test(icon, "icone da janela");
+    al_set_display_icon(disp, icon);
+    al_set_window_title(disp, "Snow Impact");
 
     // Variaveis de ambiente
     ALLEGRO_EVENT event; // Eento atual
     unsigned char key[ALLEGRO_KEY_MAX]; // Teclas
-    bool finished = false; // Diz se a execucao finalizou
-
-    // Setar o nome e icone da janela
-    al_set_display_icon(disp, icon);
-    al_set_window_title(disp, "Snow Impact");
+    memset(key, 0, sizeof(key));
 
     // Registrar fonte de eventos (teclado, tela e tempo de atualizacao)
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(disp));
     al_register_event_source(queue, al_get_timer_event_source(timer));
 
-    // Inicializar variaveis de execucao
+    bool finished = false; // Diz se a execucao finalizou
+
     al_start_timer(timer); // Inicio do timer do jogo
-
-    float x = 50, y = 50;
-    float speed = 10;
-
     while(!finished) { // Loop de jogo
         al_wait_for_event(queue, &event);
 
@@ -55,26 +54,21 @@ int main(void) {
 
             // Atualizacao de FRAME
             case ALLEGRO_EVENT_TIMER:
-                if(key[ALLEGRO_KEY_UP])
-                    y -= speed;
-                if(key[ALLEGRO_KEY_DOWN])
-                    y += speed;
-                if(key[ALLEGRO_KEY_LEFT])
-                    x -= speed;
-                if(key[ALLEGRO_KEY_RIGHT])
-                    x += speed;
-
                 al_clear_to_color(al_map_rgb(0, 0, 0));
 
-                al_draw_bitmap(player, x, y, 0);
+                if(key[ALLEGRO_KEY_UP])
+                    movement_player(&player, 0.0, -1.0);
+                if(key[ALLEGRO_KEY_RIGHT])
+                    movement_player(&player, 1.0, 0.0);
+                if(key[ALLEGRO_KEY_DOWN])
+                    movement_player(&player, 0.0, 1.0);
+                if(key[ALLEGRO_KEY_LEFT])
+                    movement_player(&player, -1.0, 0.0);
 
-                // al_draw_text(font, al_map_rgb(255, 255, 255), (X_SCREEN >> 1), (Y_SCREEN >> 1), ALLEGRO_ALIGN_CENTRE, "Press ENTER to Continue!");
-                al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 0, 0, "x: %.2f, Y: %.2f", x, y);
+                draw_player(player);
+                al_draw_textf(font, al_map_rgb(120, 120, 120), 0, 0, 0, "x: %.2f, Y: %.2f", player.x, player.y);
+
                 al_flip_display(); // Flipar para nova tela
-
-                if(key[ALLEGRO_KEY_ESCAPE])
-                    finished = true;
-
                 for(int i = 0; i < ALLEGRO_KEY_MAX; i++)
                     key[i] &= KEY_SEEN;
 
@@ -97,8 +91,9 @@ int main(void) {
     al_destroy_display(disp);
     al_destroy_timer(timer);
     al_destroy_event_queue(queue);
-    
-    al_destroy_bitmap(player);
+
+    al_destroy_audio_stream(music);
+    destroy_player(player);
     al_destroy_bitmap(icon);
 
     return 0;
